bca54b69e33bc51d98078f838ba94b7e860b3300
JaininV <jaininvekariya@gmail.com>
2025-05-22 14:16:09 -0400

    database connected

--- db_connection.py
+++ db_connection.py
@@ -1,2 +1,18 @@
-import mysql
+import mysql.connector
 
+config = {
+    'user': 'root',
+    'password': 'Jainin@0511',
+    'host': 'localhost',
+    'database': 'fbb_database'
+}
+
+def database_connection(config):
+    try:
+        connection = mysql.connector.connect(**config)
+        cursor = connection.cursor(buffered=True)
+        return "database connected!"
+    except mysql.connector.Error as err:
+        return err
+
+database_connection(config)
