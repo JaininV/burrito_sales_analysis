07469c095862978a345860f420cb50b96714a760
JaininV <jaininvekariya@gmail.com>
2025-05-23 14:29:07 -0400

    Code is ready for add employe data in database

--- models/employe.py
+++ models/employe.py
@@ -20,9 +20,15 @@ def getEmployeDataApi():
 # add values in employe
 def addEmployeDataApi(data):
     cursor = connection.cursor()
-    name = data['name']
+    f_name = data['first_name']
+    l_name = data['last_name']
     position = data['role']
     salary_hr = data['salary_hourly']
+    email = data['email_id']
+    phone_number = data['phone_number']
+    address = data['address']
+    postal = data['postal_code']
+
 
     cursor.execute("SELECT * FROM employe WHERE name = %s AND active = %s", (name, 1))
     check = cursor.fetchone()
@@ -32,8 +38,8 @@ def addEmployeDataApi(data):
         current_datetime = datetime.datetime.now()
         formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-        query = "INSERT INTO employe (name, role, salary_hourly, created_at, updated_at) VALUES (%s, %s, %s, %s, %s)"
-        value = (name, position, salary_hr, formatted_datetime, formatted_datetime)
+        query = "INSERT INTO employe (first_name, last_name, role, salary_hourly, email_id, phone_number, address, postal_code, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        value = (f_name, l_name, position, salary_hr, email, phone_number, address, postal, formatted_datetime, formatted_datetime)
 
         cursor.close()
         connection.close()
