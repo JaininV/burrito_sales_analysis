7ad34faad89384c92c887969e4d2934caf2d22e6
JaininV <jaininvekariya@gmail.com>
2025-05-27 14:59:55 -0400

    data is ready for menu

--- controller/employe.py
+++ controller/employe.py
@@ -1,18 +1,107 @@
-#Import users function from model folder
 from flask import Flask, render_template, jsonify, request
-from models.employe import getEmployeDataApi, addEmployeDataApi, upadteEmployeDataApi
-import asyncio
+from db_connection import connection, cursor
+import datetime
+import json
 
-def employeData():
-    data = getEmployeDataApi()
-    return data
+# Simulated async function
+def getEmployeDataApi():
+    query = "SELECT * FROM employe"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.commit()
+    cursor.close()
+    connection.close()
 
-def addEmployeData():
-    data = request.form
-    page = addEmployeDataApi(data)
-    return page
+    return {
+        'data': result
+    }
 
-def updateEmployeData():
-    data = request.form
-    page = upadteEmployeDataApi(data)
-    return page
+# add values in employe
+def addEmployeDataApi(data):
+    cursor = connection.cursor()
+    f_name = data['first_name']
+    l_name = data['last_name']
+    user_id = "_".join([f_name, l_name])
+    position = data['role']
+    salary_hr = data['salary_hourly']
+    email = data['email_id']
+    phone_number = data['phone_number']
+    address = data['address']
+    postal = data['postal_code']
+
+
+    cursor.execute("SELECT * FROM employe WHERE first_name = %s AND last_name = %s AND user_id = %s AND active = %s", (f_name, l_name, user_id, 1))
+    check = cursor.fetchone()
+    connection.commit()
+
+    if check is None:
+        current_datetime = datetime.datetime.now()
+        formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+
+        query = "INSERT INTO employe (first_name, last_name, user_id, role, salary_hourly, email_id, phone_number, address, postal_code, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        value = (f_name, l_name, user_id, position, salary_hr, email, phone_number, address, postal, formatted_datetime, formatted_datetime)
+
+        try:
+            cursor.execute(query, value)
+            connection.commit()
+
+            return 'Data added'
+        
+        except Exception as e:
+            return f"Error: {str(e)}"
+
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        return 'Employe already working'
+
+def upadteEmployeDataApi(data):
+    try:
+        cursor = connection.cursor()
+        f_name = data['first_name']
+        l_name = data['last_name']
+        user_id = data['user_id']
+        position = data['role']
+        salary_hr = data['salary_hourly']
+        email = data['email_id']
+        phone_number = data['phone_number']
+        address = data['address']
+        postal = data['postal_code']
+
+        check_query = "SELECT * FROM employe WHERE user_id = %s"
+        check_value = (user_id)
+        cursor.execute("SELECT * FROM employe WHERE user_id = %s AND active = %s", (user_id, 1))
+        check_result = cursor.fetchone()
+        connection.commit()
+
+        if check_result:
+            current_datetime = datetime.datetime.now()
+            formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+            user_id = "_".join([f_name, l_name])
+            query = "UPDATE employe SET first_name = %s, last_name = %s, user_id = %s, role = %s, salary_hourly = %s, email_id = %s, phone_number = %s, address = %s, postal_code = %s, updated_at = %s WHERE user_id = %s"
+            value = (f_name, l_name, user_id, position, salary_hr, email, phone_number, address, postal, formatted_datetime, user_id)
+            cursor.execute(query, value)
+            connection.commit()
+            
+            return {
+                'status': 'sucess',
+                'message': 'Data update sucessfully'
+            }
+        
+        else:
+            return{
+                'status': 'sucess',
+                'message': 'Employe Not Found'
+            }
+
+    except Exception as err:
+        return {
+            'status': 'error',
+            'message': str(err)
+        }
+    
+    finally:
+        cursor.close()
+        connection.close()
---
+++ models/menu.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, jsonify, request
+from db_connection import connection, cursor
+import datetime
+import json
+import random
+
+# Simulated async function
+def getInventoryDataApi():
+    query = "SELECT * FROM inventory"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return {
+        'data': result
+    }
+
+def addWholeDataApi():
+    cursor = connection.cursor()
+    current_datetime = datetime.datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    query = "INSERT INTO inventory (name, description, category, cost_price, sale_price, created_at, updated_at) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    vales = (
+        ('small veggie burrito', 'small veggie burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small tofu burrito', 'small tofu burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small sweet potato burrito', 'small sweet potato burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small rosted chicken burrito', 'small rosted chicken burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small breaded chicken burrito', 'small breaded chicken burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small ground beef burrito', 'small ground beef burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small pulled beef burrito', 'small pulled beef burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small pork burrito', 'small pork burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small fish burrito', 'small fish burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small shrimp burrito', 'small shrimp burrito', 'small burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big veggie burrito', 'big veggie burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big tofu burrito', 'big tofu burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big sweet potato burrito', 'big sweet potato burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big rosted chicken burrito', 'big rosted chicken burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big breaded chicken burrito', 'big breaded chicken burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big ground beef burrito', 'big ground beef burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big pulled beef burrito', 'big pulled beef burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big pork burrito', 'big pork burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big fish burrito', 'big fish burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('big shrimp burrito', 'big shrimp burrito', 'big burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge veggie burrito', 'huge veggie burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge tofu burrito', 'huge tofu burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge sweet potato burrito', 'huge sweet potato burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge rosted chicken burrito', 'huge rosted chicken burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge breaded chicken burrito', 'huge breaded chicken burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge ground beef burrito', 'huge ground beef burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge pulled beef burrito', 'huge pulled beef burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge pork burrito', 'huge pork burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge fish burrito', 'huge fish burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('huge shrimp burrito', 'huge shrimp burrito', 'huge burrito', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl veggie', 'bowl veggie', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl tofu', 'bowl tofu', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl sweet potato', 'bowl sweet potato', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl rosted chicken', 'bowl rosted chicken', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl breaded chicken', 'bowl breaded chicken', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl ground beef', 'bowl ground beef', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl pulled beef', 'bowl pulled beef', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl pork', 'bowl pork', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl fish', 'bowl fish', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('bowl shrimp', 'bowl shrimp', 'bowl', random.randint(10,12), random.randint(12,17), formatted_datetime, formatted_datetime)
+        ('small fries', 'small fries', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('large fries', 'large fries', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('chips and salsa', 'chips and salsa', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('chips and guac', 'chips and guac', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('chips and queso', 'chips and queso', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('chicken empanada', 'chicken empanada', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('ground beef empanada', 'small fries', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('mars bar', 'mars bar', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('cheesecake burrito', 'cheese cake burrito', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('churros', 'churros', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('loaded fries', 'loaded fries', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+        ('loaded nachos', 'loaded nachos', 'side', random.randint(7,8), random.randint(8,11), formatted_datetime, formatted_datetime)
+    )
+
+    cursor.executemany(query, vales)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return {
+            'status': 'sucess',
+            'message': 'Data added'
+        }
