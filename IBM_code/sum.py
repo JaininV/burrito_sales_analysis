ae99d59df09254dc5ecef728c0214167a83b4028
JaininV <jaininvekariya@gmail.com>
2025-06-04 13:05:00 -0400

    code done for sum digit of number

---
+++ IBM_code/sum.py
@@ -0,0 +1,13 @@
+num = int(input("Enter nummber: "))
+
+def convert_number_iin_digit(data):
+    n = data
+    sum = 0
+    while n > 0:
+        i = n%10
+        sum = i + sum
+        n = int(n/10)
+    
+    return sum
+result = convert_number_iin_digit(num)
+print(result)
