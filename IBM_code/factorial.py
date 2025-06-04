c98b197380ef9a3513acb1db4074cd7931c5d612
JaininV <jaininvekariya@gmail.com>
2025-06-04 02:33:14 -0400

    get factorial of given number

---
+++ IBM_code/factorial.py
@@ -0,0 +1,13 @@
+# get input by user
+def factorial(number):
+    x = number
+    fac = 1
+    while 1 <= number:
+        fac = fac*number
+        number = number - 1
+    
+    return f"Factorial of {x} is {fac}."
+
+num = int(input("Enter your desire number: "))
+result = factorial(num)
+print(result)
