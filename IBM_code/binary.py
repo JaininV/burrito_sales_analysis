5693e88183fb1e7e95076a6121a061535390ab3f
JaininV <jaininvekariya@gmail.com>
2025-06-04 15:21:20 -0400

    complete IBM code

---
+++ IBM_code/binary.py
@@ -0,0 +1,14 @@
+num = int(input("Enter your number: "))
+
+def convert_binary(data):
+    bin = []
+    n = data    
+    while n>0:
+        i = n%2
+        bin.append(i)
+        n = int(n /2)
+    
+    return bin
+
+result = convert_binary(num)
+print(result)
