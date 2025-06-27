60c46667008b1bd21625cfc3cf0070cbe6e470c9
JaininV <jaininvekariya@gmail.com>
2025-06-27 15:38:34 -0400

    code is set for k frequency

--- IBM_code/top_k_frequency.py
+++ IBM_code/top_k_frequency.py
@@ -1,18 +1,35 @@
 # Enter the values:
 arr = [1,1,1,2,3,1,2,5,5,4,4,5,6,4,1,2,3]
-k = input(int("Enter Frequency: "))
+k = int(input("Enter Frequency: "))
 
 # Funcion for geting frequent values:
 def most_frequent_values(num, k):
     n = 0
     l = len(num)
-    for i in range(0, l):
-        for j in range(0, l - i -1):
-            n = num[i]
-            num[i] = num[j]
-            num[j] = n
+    for i in range(l-1):
+        for j in range(l - i -1):
+            if num[j] > num[j+1]:
+                n = num[j]
+                num[j] = num[j+1]
+                num[j+1] = n
     
-    print(num)
+    # Make disc for frequency of each element
+    disc = {}
+    for i in range(l):
+        x = num[i]
+        if x in disc:
+            disc[x] = disc[x] + 1
 
+        else:
+            disc[x] = 1
+    
+    # return output
+    result = {}
+    for i in disc:
+        if disc[i] >= k:
+            result[i] = disc[i]
+    
+    return result
 # call funtion
-most_frequent_values()
+result = most_frequent_values(arr, k)
+print(result)
